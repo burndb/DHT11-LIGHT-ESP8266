dd7ad6c05f93155401c3ce390ecbf945359a4dff
Domenic Bremmer <fdm.bremmer@gmail.com>
2022-07-13 11:08:07 +0200

    temp+humidity

--- src/main.cpp
+++ src/main.cpp
@@ -12,119 +12,37 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Hash.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssidap     = "ESP8266-Access-Point";
-const char* passwordap = "123456789";
 const char* ssid = "NETGEAR";
 const char* password = "";
 
 #define DHTPIN D3    // Digital pin connected to the DHT sensor
-
-// Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
-//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
-
 DHT dht(DHTPIN, DHTTYPE);
 
-String HOST_NAME = "http://192.168.211.56"; // change to your PC's IP address
-String PATH_NAME   = "/insert_temp.php";
-String queryString = "?temperature=";
+String HOST_NAME = "http://192.168.211.56"; // apache server
+String PATH_NAME   = "/insert_temp.php";  //php script
+String queryTemp = "?temperature=";
+String queryHum = "?humidity=";
+String queryLight = "?light=";
 WiFiClient wifiClient;
 HTTPClient http;
 // current temperature & humidity, updated in loop()
 float t = 0.0;
 float h = 0.0;
+int l = 0;
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
 unsigned long previousMillis = 0;    // will store last time DHT was updated
 
 // Updates DHT readings every 10 seconds
 const long interval = 10000;  
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 15px;
-    }
-  </style>
-</head>
-<body>
-  <h2>ESP8266 DHT Server</h2>
-  <p>
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup>
-  </p>
-  <p>
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity">%HUMIDITY%</span>
-    <sup class="units">%</sup>
-  </p>
-</body>
-<script>
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 10000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("humidity").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/humidity", true);
-  xhttp.send();
-}, 10000 ) ;
-</script>
-</html>)rawliteral";
-
-// Replaces placeholder with DHT values
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "TEMPERATURE"){
-    return String(t);
-  }
-  else if(var == "HUMIDITY"){
-    return String(h);
-  }
-  return String();
-}
-
 void post_temp() {
-  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryString + newT)); //HTTP
-    Serial.print(HOST_NAME + PATH_NAME + queryString + newT);
+  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryTemp + t)); //HTTP
+    Serial.print(HOST_NAME + PATH_NAME + queryTemp + t);
     int httpCode = http.GET();
 
   // httpCode will be negative on error
@@ -146,17 +64,57 @@ void post_temp() {
       http.end();
 }
 
+void post_hum() {
+  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryHum + h)); //HTTP
+    Serial.print(HOST_NAME + PATH_NAME + queryHum + h);
+    int httpCode = http.GET();
+
+  // httpCode will be negative on error
+      if(httpCode > 0) {
+    // file found at server
+        if(httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+        } 
+        else {
+      // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+        }
+      } 
+      else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+}
+
+void post_light() {
+  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryLight + l)); //HTTP
+    Serial.print(HOST_NAME + PATH_NAME + queryLight + h);
+    int httpCode = http.GET();
+
+  // httpCode will be negative on error
+      if(httpCode > 0) {
+    // file found at server
+        if(httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+        } 
+        else {
+      // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+        }
+      } 
+      else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+}
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
   dht.begin();
-  
-  Serial.print("Setting AP (Access Point)…");
-  // Remove the password parameter, if you want the AP (Access Point) to be open
-  //WiFi.softAP(ssidap, passwordap); //softAP
-  //IPAddress IP = WiFi.softAPIP(); //softAP
-  //Serial.print("AP IP address: "); //softAP
-  //Serial.println(IP); //softAP
 
   WiFi.begin(ssid);
   Serial.println("Connecting to WiFi");
@@ -166,20 +124,6 @@ void setup(){
   }
   // Print ESP8266 Local IP Address
   Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(t).c_str());
-  });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", String(h).c_str());
-  });
-
-  // Start server
-  server.begin();
 }
  
 void loop(){  
@@ -189,9 +133,9 @@ void loop(){
     previousMillis = currentMillis;
 
     //read and print light value
-    int light = (int)analogRead(A0);
+    l = (int)analogRead(A0);
     Serial.print("Lichtwert ADC: ");
-    Serial.println(light);
+    Serial.println(l);
 
     // Read temperature as Celsius (the default)
     float newT = dht.readTemperature();
@@ -213,5 +157,6 @@ void loop(){
       Serial.println(h);
     }
     post_temp();
+    post_hum();
   }
 }

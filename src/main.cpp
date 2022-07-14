da9365dad87d27d9ca07c6b3f393029d18fefbbf
Domenic Bremmer <fdm.bremmer@gmail.com>
2022-07-14 12:43:23 +0200

    noserver, one request

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,3 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com/esp8266-nodemcu-access-point-ap-web-server/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-
-// Import required libraries
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Hash.h>
@@ -24,10 +13,11 @@ const char* password = "";
 DHT dht(DHTPIN, DHTTYPE);
 
 String HOST_NAME = "http://192.168.211.56"; // apache server
-String PATH_NAME   = "/insert_temp.php";  //php script
-String queryTemp = "?temperature=";
-String queryHum = "?humidity=";
-String queryLight = "?light=";
+String PATH_NAME   = "/insert_temp.php?";  //php script
+String queryTemp = "temperature=";
+String queryHum = "humidity=";
+String queryLight = "light=";
+String andOperator = "&";
 WiFiClient wifiClient;
 HTTPClient http;
 // current temperature & humidity, updated in loop()
@@ -40,77 +30,27 @@ unsigned long previousMillis = 0;    // will store last time DHT was updated
 // Updates DHT readings every 10 seconds
 const long interval = 10000;  
 
-void post_temp() {
-  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryTemp + t)); //HTTP
-    Serial.print(HOST_NAME + PATH_NAME + queryTemp + t);
-    int httpCode = http.GET();
-
-  // httpCode will be negative on error
-      if(httpCode > 0) {
+void post_readings() {
+  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryTemp + t + andOperator + queryHum + h + andOperator + queryLight  + l)); //HTTP
+  Serial.print(HOST_NAME + PATH_NAME + queryTemp + t + andOperator + queryHum + h + andOperator + queryLight  + l);
+  int httpCode = http.GET();
+  if(httpCode > 0) {
     // file found at server
-        if(httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        Serial.println(payload);
-        } 
-        else {
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } 
+    else {
       // HTTP header has been send and Server response header has been handled
           Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-        }
-      } 
-      else {
+    }
+  } 
+  else {
         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-
+  }
       http.end();
 }
 
-void post_hum() {
-  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryHum + h)); //HTTP
-    Serial.print(HOST_NAME + PATH_NAME + queryHum + h);
-    int httpCode = http.GET();
-
-  // httpCode will be negative on error
-      if(httpCode > 0) {
-    // file found at server
-        if(httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        Serial.println(payload);
-        } 
-        else {
-      // HTTP header has been send and Server response header has been handled
-          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-        }
-      } 
-      else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-
-      http.end();
-}
-
-void post_light() {
-  http.begin(wifiClient, (HOST_NAME + PATH_NAME + queryLight + l)); //HTTP
-    Serial.print(HOST_NAME + PATH_NAME + queryLight + h);
-    int httpCode = http.GET();
-
-  // httpCode will be negative on error
-      if(httpCode > 0) {
-    // file found at server
-        if(httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        Serial.println(payload);
-        } 
-        else {
-      // HTTP header has been send and Server response header has been handled
-          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-        }
-      } 
-      else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-
-      http.end();
-}
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
@@ -144,6 +84,7 @@ void loop(){
     }
     else {
       t = newT;
+      Serial.println("Temperature: ");
       Serial.println(t);
     }
     // Read Humidity
@@ -154,9 +95,9 @@ void loop(){
     }
     else {
       h = newH;
+      Serial.println("Humidity: ");
       Serial.println(h);
     }
-    post_temp();
-    post_hum();
+    post_readings();
   }
 }
